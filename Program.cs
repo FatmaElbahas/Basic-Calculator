bbe016ab3063625e74e12d5ec4eea827353152ca
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-07 23:47:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp107
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num1");
+            double num1=Convert.ToInt64 (Console.ReadLine());
+            Console.WriteLine("Enter num2");
+            double num2 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("sumation "+ Convert.ToDouble(num1+num2));
+            Console.WriteLine("Subtraction " + Convert.ToDouble(num1-num2));
+            Console.WriteLine("Division " + Convert.ToDouble(num1 / num2));
+            Console.WriteLine("Multiplication " + Convert.ToDouble(num1 * num2));
+
+        }
+    }
+}
